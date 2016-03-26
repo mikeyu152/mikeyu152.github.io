6207315799c7f76a8595699c1f3091d20642e53b
mikeyu152 <myu3@stanford.edu>
2016-03-25 19:22:59 -0700

    implemented img preload

--- js/imgpreload.js
+++ js/imgpreload.js
@@ -1,2 +1,26 @@
 blendcolor = new Image(1602,402)
 blendcolor.src = "img/experience/blendcolor.jpg"
+
+nearpodcolor = new Image(1600,40)
+nearpodcolor.src = "img/experience/nearpodcolor.jpg"
+
+kairoscolor = new Image(1600,40)
+kairoscolor.src = "img/experience/kairoscolor.jpg"
+
+treehackscolor = new Image(1600,40)
+treehackscolor.src = "img/experience/treehackscolor.jpg"
+
+janestreetcolor = new Image(1600,40)
+janestreetcolor.src = "img/experience/janestreetcolor.jpg"
+
+bloombergcolor = new Image(1600,40)
+bloombergcolor.src = "img/experience/bloombergcolor.jpg"
+
+stanford2025color = new Image(1600,40)
+stanford2025color.src = "img/experience/stanford2025color.jpg"
+
+stanfordcolor = new Image(1596,40)
+stanfordcolor.src = "img/experience/stanfordcolor.jpg"
+
+waterresearchcolor = new Image(1596,40)
+waterresearchcolor.src = "img/experience/waterresearchcolor.jpg"
